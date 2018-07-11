8326ca0e72d75fd0c4a7470e02d4053ae929573f
Joe Howard <succinction@gmail.com>
2018-07-11 16:19:43 -0700

    socket connected

--- server/server.js
+++ server/server.js
@@ -1,17 +1,46 @@
-const path = require('path')
 const express = require('express')
+const path = require('path')
+const http = require('http');
+//make sure you keep this order
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+var port = process.env.PORT || 3000
 
 const publicPath = path.join(__dirname, '../public')
-const port = process.env.PORT || 3000
-
-
-const app = express()
 
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
-    console.log("App running on port " + port)
+server.listen(port, () => {
+    console.log('listening on port  ' + port)
+});
+
+io.on('connection', (socket) => {
+    console.log('new ueser connected')
+    socket.on('disconnect', () => {
+        console.log('User was disconnected')
+
+    })
 })
+
+
+// const express = require('express') 
+// const path = require('path')
+// const http = require('http')
+// const socketIO = require('socket.io')
+
+// const port = process.env.PORT || 3000
+
+// const app = express()
+// app.listen(port, () => {
+//     console.log("App running on port " + port)
+// })
+// var server = http.createServer(app)
+// var io = socketIO.listen(server)
+
+// app.use(express.static(publicPath))
+
 
 
 
